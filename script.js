40e4c367b959cdd276052a723dc701e443ca3ec7
koichi-inoue <inoue.ko@gmail.com>
2019-08-27 18:37:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+navigator.getUserMedia = navigator.getUserMedia
+    || navigator.webkitGetUserMedia
+    || navigator.mozGetUserMedia;
+
+window.URL = window.URL || window.webkitURL;
+
+let webcam = document.getElementById('webcam');
+let localStream = null;
+
+navigator.getUserMedia( {video: true, audio: false}, success, error);
+
+
+function success(stream) { // for success case
+  webcam.srcObject = stream;
+  console.log(stream);
+}
+
+function error(err) { // for error case
+	console.log(err);
+}
